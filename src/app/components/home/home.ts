5ea4127920c76c3b3c4053bf66de62c1e509605b
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2026-01-07 15:24:13 -0400

    feat: Implement new home page with hero section, feature highlights, and iOS install modal.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styles: ``,
 })
 export class HomeComponent {
+  showIosModal = false;
 
+  toggleIosModal() {
+    this.showIosModal = !this.showIosModal;
+  }
 }
